2889ed3a09c3729622b8f914792b2b062c130b45
s2ungja2 <s2ungja2@naver.com>
2018-08-07 09:23:03 +0900

    truffle configuration.

--- Blockchain/truffle.js
+++ Blockchain/truffle.js
@@ -15,4 +15,12 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+
+  network: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  }
 };
