90996ab6a5563262c05a3ec1bd4d764c34ea2e0e
s2ungja2 <s2ungja2@naver.com>
2018-08-09 15:50:03 +0900

    Create Contract 'DonationCoin'

--- Blockchain/truffle.js
+++ Blockchain/truffle.js
@@ -13,14 +13,12 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-
-  network: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*"
-    }
-  }
+	networks: {
+		development :{
+			host: '127.0.0.1',
+			port: 7545,
+			network_id: '*',
+		}
+	}
 };
+
