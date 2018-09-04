a14f0078bd41a2527eca59e2b0cc98b33ff8691b
begaonnuri <begaonnuri@gmail.com>
2018-09-04 17:55:19 +0900

    mainactivity, donation group list update

--- Android/app/src/main/java/com/example/nam/donationmanager/GroupSelect.java
+++ Android/app/src/main/java/com/example/nam/donationmanager/GroupSelect.java
@@ -1,16 +1,20 @@
 package com.example.nam.donationmanager;
 
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GroupSelect extends AppCompatActivity {
 
-    final int ITEM_SIZE = 4;
+    final int ITEM_SIZE = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,10 @@ public class GroupSelect extends AppCompatActivity {
         item[1] = new Item(R.drawable.neighbors, "굿네이버스");
         item[2] = new Item(R.drawable.redcross, "대한적십자사");
         item[3] = new Item(R.drawable.kfhi, "기아대책");
+        item[4] = new Item(R.drawable.kfhi, "가나");
+        item[5] = new Item(R.drawable.kfhi, "다라");
+        item[6] = new Item(R.drawable.kfhi, "마바");
+        item[7] = new Item(R.drawable.kfhi, "사아자");
 
         for (int i = 0; i < ITEM_SIZE; i++) {
             items.add(item[i]);
