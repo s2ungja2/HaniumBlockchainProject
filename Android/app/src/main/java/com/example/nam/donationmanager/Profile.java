8e9b3478ae8852fe5ade844c89f163bdb530a0e7
begaonnuri <begaonnuri@gmail.com>
2018-09-28 13:56:59 +0900

    complete token change activity

--- Android/app/src/main/java/com/example/nam/donationmanager/Profile.java
+++ Android/app/src/main/java/com/example/nam/donationmanager/Profile.java
@@ -26,7 +26,6 @@ public class Profile extends AppCompatActivity{
         name = findViewById(R.id.profile_name);
         email = findViewById(R.id.profile_email);
         sex = findViewById(R.id.profile_sex);
-        rank = findViewById(R.id.profile_rank);
         count = findViewById(R.id.profile_count);
         money = findViewById(R.id.profile_money);
         token = findViewById(R.id.profile_token);
@@ -46,17 +45,16 @@ public class Profile extends AppCompatActivity{
                     UserRepo repo = response.body();
                     List<UserRepo.Result> datalist = repo.result;
 
-                    id.setText(datalist.get(0).user_id.toString());
-                    name.setText(datalist.get(0).user_name.toString());
-                    email.setText(datalist.get(0).user_email.toString());
-                    if(datalist.get(0).user_sex == false)
+                    id.setText(datalist.get(1).user_id.toString());
+                    name.setText(datalist.get(1).user_name.toString());
+                    email.setText(datalist.get(1).user_email.toString());
+                    if(datalist.get(1).user_sex == false)
                         sex.setText("남자");
                     else
                         sex.setText("여자");
-                    //rank.setText(""+datalist.get(0).user_rank);
-                    count.setText(""+datalist.get(0).user_count);
-                    money.setText(""+datalist.get(0).user_money);
-                    token.setText(""+datalist.get(0).user_token);
+                    count.setText(""+datalist.get(1).user_count);
+                    money.setText(""+datalist.get(1).user_money);
+                    token.setText(""+datalist.get(1).user_token);
                 }
             }
 
